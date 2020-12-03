3f52ac4edb53808425f0248825567fca0df7847a
Mohit Kumar <mohit56779@gmail.com>
2020-12-03 06:29:24 -0700

    adds animation for navigation

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,25 +2,29 @@ import React from 'react';
 import {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import {useTransition, animated} from 'react-spring'
+
 
 
 function Navigation() {
 	const [showMenu, setShowMenu] = useState(false);
-	let menu;
-	let menuMask;
 
-	if(showMenu){
-		menu = 
-		<div className = 'fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow'>
-			The menu
-		</div>
+    // for nav animation
+	const menuTransitions = useTransition(showMenu, null, {
+		from: {  opacity: 0, transform: 'translateX(-100%)' },
+		enter: { opacity: 1, transform: 'translateX(0%)' },
+		leave: { opacity: 0,  transform: 'translateX(-100%)' },
+    })
 
-		menuMask = 
-		<div className = 'bg-black-t-50 fixed top-0 left-0 w-full h-full z-50'
-			onClick = {() => setShowMenu(false)}>
+    const maskTransitions = useTransition(showMenu, null, {
+		from: { position: 'absolute', opacity: 0 },
+		enter: { opacity: 1 },
+		leave: { opacity: 0 },
+    })
 
-		</div>
-	}
+    //className = 'fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow'
+
+    // className = 'bg-black-t-50 fixed top-0 left-0 w-full h-full z-50
 
 	return(
 		<nav>
@@ -32,8 +36,40 @@ function Navigation() {
 
 			</span>
 
-			{menuMask}
-			{menu}
+
+		    {maskTransitions.map(({ item, key, props }) =>
+				item &&
+				 <animated.div
+				  key={key} 
+				  style={props}
+				  className = 'bg-black-t-50 fixed top-0 left-0 w-full h-full z-50'
+				  onClick = {() => setShowMenu(false)}>
+
+				  </animated.div>
+		  )}
+
+		  {menuTransitions.map(({ item, key, props }) =>
+				item &&
+				 <animated.div
+				  key={key} 
+				  style={props}
+				  className = 'fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow'
+				  >
+
+				  <span className = 'font-bold'>
+				  	Menu
+				  </span>
+
+				  <ul>
+				  	  <li> Home </li>
+
+				  </ul>
+
+				  </animated.div>
+		  )}
+
+
+
 
 		</nav>
 		)
