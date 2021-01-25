1d5edc1ef1b60a7458bbad952336a36bcbda285c
Mohit Kumar <mohit56779@gmail.com>
2021-01-25 14:45:44 -0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import React from 'react';
 import HelloWorld from './Components/HelloWorld'
 import AppHeader from './Components/AppHeader'
 import Footer from './Components/Footer'
+import {
+	BrowserRouter as Router,
+            Switch,
+            Route,
+            Link
+} from 'react-router-dom';
+
 
 function App() {
   return (
     <div>
-      <AppHeader/> 
+      
+      <Router>
 
+      <AppHeader/> 
       <HelloWorld name= "Mohit"/>
 
+      <ul>
+      	<li>
+      		<Link to = "/" classnName = "text-blue-500"> Home </Link>
+      	</li>
+      </ul>
+
       <Footer/>
+
+      </Router>
       
     </div>
   );
