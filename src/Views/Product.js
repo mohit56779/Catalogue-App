fbe603c8854afd2348b78744faa648d833ba70e5
Mohit <mohit56779@gmail.com>
2021-02-25 20:49:39 -0700

    added loading image

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,39 +1,52 @@
 import React from 'react'
 import axios from 'axios'
+import Loader from '../Components/Loader'
 import {useState,useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 
 function Product(){
     const {id} = useParams()
     const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/${id}`
-    const [product,setProduct] = useState(null)
+    const [product,setProduct] = useState({
+        loading:false,
+        data:null
+    })
 
     let content = null
     useEffect( () => {
+        setProduct({
+            loading:true,
+            data:null,
+        })
         axios.get(url).then(response => {
-        setProduct(response.data)
+        setProduct({
+            loading: false,
+            data: response.data})
     })
     }, [url])
 
+    if(product.loading){
+        content = <Loader/> 
+    }
 
-    if(product){
+    if(product.data){
         content = 
         <div>
             <h1 className="text-2xl font-bold mb-3">
-                {product.name}
+                {product.data.name}
             </h1>
 
             <div>
-                <img src = {product.images[0].imageUrl}
-                alt = {product.name}/>
+                <img src = {product.data.images[0].imageUrl}
+                alt = {product.data.name}/>
             </div>
 
             <div className= "font-bold text-xl mb-3">
-                $ {product.price}
+                $ {product.data.price}
             </div>
 
             <div>
-                {product.description}
+                {product.data.description}
             </div>
         </div>
     }
